9de77586942b6a14e6b68c0ffe3ee88da488f189
Lucas Araújo Amorim <eligadowb@gmail.com>
2023-11-24 18:38:15 -0400

    icon fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import './global.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <link rel="icon" href="/favicon.ico" />
       <body>{children}</body>
     </html>
   )
